c7c5e76bdef85dae0511a8b46d51ef1419ab8889
mp-104 <mp-104@hotmail.com>
2024-10-24 15:59:28 +0200

    attempt at card design

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,34 @@ export default function Home() {
         </div>
 
       </div>
+
+      
+      <div className="h-screen bg-slate-600 flex justify-center items-center">
+        <p>Hallo där</p>
+
+        {/** Card Design */}
+        <div className="bg-cyan-300 max-w-sm md:flex-col">
+          
+          <img src="https://upload.wikimedia.org/wikipedia/commons/f/f6/Roundel_of_Russia.svg" 
+          alt="air force"
+          className="h-80 w-80 object-fill" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur amet incidunt dolores omnis ad illum sequi modi porro ducimus totam id sit, recusandae maxime, quia tenetur aliquid, perferendis autem possimus!</p>
+          
+        </div>
+
+        {/*
+        <div  className="bg-cyan-300 max-w-sm ">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Coat_of_arms_of_the_Soviet_Union_%281956%E2%80%931991%29.svg/1024px-Coat_of_arms_of_the_Soviet_Union_%281956%E2%80%931991%29.svg.png" 
+          className="h-full w-full object-fill"
+          />
+
+        </div>
+        */}
+        
+        
+      </div>
+    
+
     </>
   );
 }
